c8dd5407be58ea6f8b75f964c05952c89785506b
Rhea Parekh <rheaparekh12@gmail.com>
2019-11-05 17:28:01 +0100

    Bell state measurement and make Bank receive cheque

--- quantumwinter/quantum_cheque.py
+++ quantumwinter/quantum_cheque.py
@@ -9,7 +9,7 @@ db_id = 1
 pk = 3
 sk = 4
 M = 400
-
+cheque = []
 
 def one_way_function(conn, BB84_key, db_id, r, M):
     owf_key = bin(BB84_key)[2:] + bin(db_id)[2:] + bin(r)[2:] + bin(M)[2:]
@@ -26,6 +26,7 @@ def measure(conn, q):
     print("|"+"-"*(len(to_print)+2)+"|")
     print("| "+to_print+" |")
     print("|"+"-"*(len(to_print)+2)+"|")
+    return m
 
 
 class ThreadAlice(Thread):
@@ -38,21 +39,34 @@ class ThreadAlice(Thread):
             r = randint(0, 1)
             owf_state = one_way_function(Alice, BB84_key, db_id, r, M)
 
-            Alice.sendQubit(qforC, "Charlie")
-
             # Bell state measurement 
             owf_state.cnot(qA)
             owf_state.H()
 
-            measure(Alice, qA)
-            measure(Alice, owf_state)
+            qA_measure_result = measure(Alice, qA)
+            owf_state_measure_result = measure(Alice, owf_state)
+
+            if qA_measure_result == 0 and owf_state_measure_result == 1:
+                qforC.Z()
+            if qA_measure_result == 1 and owf_state_measure_result == 0:
+                qforC.X()
+            if qA_measure_result == 1 and owf_state_measure_result == 1:
+                qforC.Y()
+
+            # qforC qubit is now the cheque which is transferred to Charlie
+            Alice.sendQubit(qforC, "Charlie")
+            cheque.append(db_id)
+            cheque.append(r)
+            cheque.append(M)
+
 
 class ThreadCharlie(Thread):
 
     def run(self):
         with CQCConnection("Charlie") as Charlie:
             qC = Charlie.recvQubit()
-            measure(Charlie, qC)
+            #sendQubit(toto, Bob)
+            Charlie.sendQubit(qC, "Bob")
 
 class ThreadBank(Thread):
 
@@ -63,6 +77,9 @@ class ThreadBank(Thread):
             qB.cnot(qA)
             Bob.sendQubit(qA,"Alice")
             measure(Bob, qB)
+
+            qC = Bob.recvQubit()
+            measure(Bob, qC)
 
 def main():
     ThreadAlice().start()
